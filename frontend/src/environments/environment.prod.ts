24454f5a887fe205360e4903e17bcd308caab120
Nikita Zalevskii <nzalevskii@yandex.ru>
2024-12-16 20:36:34 +0100

    build frontend v1.002

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://its-26u4.onrender.com/api/',
-  serverStaticPath: 'https://its-26u4.onrender.com/images/',
+  api: 'https://bitstorm.onrender.com/api/',
+  serverStaticPath: 'https://bitstorm.onrender.com/images/',
 };
